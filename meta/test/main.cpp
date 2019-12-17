cc5900c949eddb6ded0612beff95df7a9854d2ac
Berrysoft <Strawberry_Str@hotmail.com>
2019-12-17 23:22:23 +0800

    Merge meta.

--- meta/test/main.cpp
+++ meta/test/main.cpp
@@ -10,8 +10,11 @@ class calculator : public meta_class_impl<calculator>
 public:
     static constexpr std::string_view class_name = "calculator";
 
-    int plus(int x, int y) { return x + y; }
-    int minus(int x, int y) { return x - y; }
+    PROP_EVENT(value, int)
+
+public:
+    void plus(int x, int y) { set_value(x + y); }
+    void minus(int x, int y) { set_value(x - y); }
 
     ~calculator() override {}
 
@@ -21,6 +24,7 @@ public:
         ADD_CTOR();
         ADD_METHOD(plus);
         ADD_METHOD(minus);
+        ADD_PROP_EVENT(value);
     }
 };
 
@@ -28,7 +32,11 @@ int main()
 {
     register_class<calculator>();
 
-    auto mc = construct(*get_type_index("calculator"));
-    cout << *invoke_method<int>(mc, "plus", 1, 1) << endl;
-    cout << *invoke_method<int>(mc, "minus", 1, 1) << endl;
+    auto t = *get_type_index("calculator");
+    auto mc = construct(t);
+    auto ev = get_event<calculator const&, int>(t, "value_changed");
+    auto token = ev.add(mc, function<void(calculator const&, int)>([](calculator const&, int i) { cout << "Value changed: " << i << endl; }));
+    invoke_method<void>(mc, "plus", 1, 1);
+    ev.remove(mc, token);
+    invoke_method<void>(mc, "minus", 1, 1);
 }
--- ui/test/main.cpp
+++ ui/test/main.cpp
@@ -1,39 +1,7 @@
 #include <iostream>
-#include <xaml/ui/event.hpp>
-#include <xaml/ui/meta.hpp>
-#include <xaml/ui/meta_macro.hpp>
 
 using namespace std;
-using namespace xaml;
-
-class storage : public meta_class_impl<storage>
-{
-public:
-    static constexpr std::string_view class_name = "storage";
-
-    PROP_EVENT(value, int)
-
-    ~storage() override {}
-
-    static void register_class() noexcept
-    {
-        REGISTER_TYPE();
-        ADD_CTOR();
-        ADD_PROP_EVENT(value);
-    }
-};
 
 int main()
 {
-    register_class<storage>();
-
-    auto t = *get_type_index("storage");
-    auto mc = construct(t);
-    auto ev = get_event<storage const&, int>(t, "value_changed");
-    auto token = ev.add(mc, function<void(storage const&, int)>([](storage const&, int i) { cout << "Value changed: " << i << endl; }));
-    auto prop = get_property(t, "value");
-    prop.set(mc, 100);
-    ev.invoke<storage const&, int>(mc, *reinterpret_pointer_cast<storage>(mc), 150);
-    ev.remove(mc, token);
-    prop.set(mc, 200);
 }
