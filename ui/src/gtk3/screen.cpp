6c1f9ebb74d46552b61ddb075996a83334a7387e
Berrysoft <Strawberry_Str@hotmail.com>
2019-12-19 11:23:35 +0800

    Add screen for GTK.

---
+++ ui/src/gtk3/screen.cpp
@@ -0,0 +1,34 @@
+#include <gtk/gtk.h>
+#include <xaml/ui/screen.hpp>
+
+using namespace std;
+
+namespace xaml
+{
+    unsigned int screen_dpi()
+    {
+        GdkScreen* screen = gdk_screen_get_default();
+        gdouble dpi = gdk_screen_get_resolution(screen);
+        if (dpi < 0)
+            return 96;
+        else
+            return (unsigned int)dpi;
+    }
+
+    vector<monitor> get_monitors()
+    {
+        vector<monitor> ms;
+        GdkScreen* screen = gdk_screen_get_default();
+        GdkDisplay* display = gdk_screen_get_display(screen);
+        int n = gdk_display_get_n_monitors(display);
+        for (int i = 0; i < n; i++)
+        {
+            GdkMonitor* m = gdk_display_get_monitor(display, i);
+            monitor props = {};
+            gdk_monitor_get_geometry(m, (GdkRectangle*)&props.region);
+            gdk_monitor_get_workarea(m, (GdkRectangle*)&props.client);
+            ms.push_back(move(props));
+        }
+        return ms;
+    }
+} // namespace xaml
