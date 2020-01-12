4e86ca2d440767955f6de6fdda6d0c189d0736db
Berrysoft <Strawberry_Str@hotmail.com>
2020-01-12 11:49:41 +0800

    Fix gtk3 build.

--- ui/src/gtk3/password_entry.cpp
+++ ui/src/gtk3/password_entry.cpp
@@ -1,12 +1,29 @@
+#include <gtk3/drawing.hpp>
 #include <xaml/ui/password_entry.hpp>
 
 namespace xaml
 {
     void password_entry::__draw(const rectangle& region)
     {
-        entry::__draw(region);
+        if (!get_handle())
+        {
+            set_handle(gtk_entry_new());
+        }
+        rectangle real = region - get_margin();
+        set_size({ real.width, real.height });
+        draw_text();
         gtk_entry_set_visibility(GTK_ENTRY(get_handle()), FALSE);
         draw_password_char();
+    }
+
+    void password_entry::draw_size()
+    {
+        gtk_widget_set_size_request(get_handle(), get_rwidth(get_width()), get_rheight(get_height()));
+    }
+
+    void password_entry::draw_text()
+    {
+        gtk_entry_set_text(GTK_ENTRY(get_handle()), m_text.c_str());
     }
 
     void password_entry::draw_password_char()
@@ -15,5 +32,12 @@ namespace xaml
             gtk_entry_set_invisible_char(GTK_ENTRY(get_handle()), m_password_char);
         else
             gtk_entry_unset_invisible_char(GTK_ENTRY(get_handle()));
+    }
+
+    void password_entry::__size_to_fit()
+    {
+        int width = gtk_widget_get_allocated_width(get_handle());
+        int height = gtk_widget_get_allocated_height(get_handle());
+        __set_size_noevent({ (double)width, (double)height });
     }
 } // namespace xaml
