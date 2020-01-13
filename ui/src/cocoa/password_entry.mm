72d37b018caaf5f2eb5718f1a8cb9e0f09f6eb47
Berrysoft <Strawberry_Str@hotmail.com>
2020-01-13 22:20:54 +0800

    Fix cocoa build.

--- ui/src/cocoa/password_entry.mm
+++ ui/src/cocoa/password_entry.mm
@@ -14,43 +14,11 @@ namespace xaml
             textField.selectable = YES;
             set_handle(textField);
         }
-        rectangle real = region - get_margin();
-        NSSecureTextField* textField = (NSSecureTextField*)get_handle();
-        NSView* view = get_parent()->get_handle();
-        NSRect parent_frame = [view frame];
-        NSRect frame = textField.frame;
-        frame.origin = { real.x, parent_frame.size.height - real.height - real.y };
-        frame.size = { real.width, real.height };
-        textField.frame = frame;
-        __set_size_noevent({ real.width, real.height });
-        draw_text();
+        entry::__draw(region);
         draw_password_char();
-    }
-
-    void password_entry::draw_size()
-    {
-        NSSecureTextField* textField = (NSSecureTextField*)get_handle();
-        NSRect frame = textField.frame;
-        frame.size = get_NSSize(get_size());
-        textField.frame = frame;
-    }
-
-    void password_entry::draw_text()
-    {
-        NSSecureTextField* textField = (NSSecureTextField*)get_handle();
-        NSString* ns_title = [NSString stringWithUTF8String:m_text.c_str()];
-        textField.stringValue = ns_title;
     }
 
     void password_entry::draw_password_char()
     {
-    }
-
-    void password_entry::__size_to_fit()
-    {
-        NSSecureTextField* textField = (NSSecureTextField*)get_handle();
-        [textField sizeToFit];
-        NSRect frame = textField.frame;
-        __set_size_noevent(xaml::get_size(frame.size));
     }
 }
