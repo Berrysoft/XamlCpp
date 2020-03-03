80c886965fb54d37afec5f180dca1ec1f63b8080
Berrysoft <Strawberry_Str@hotmail.com>
2020-03-03 15:03:37 +0800

    Add progress for cocoa.

---
+++ ui_controls/src/cocoa/progress.mm
@@ -0,0 +1,28 @@
+#include <xaml/ui/controls/progress.hpp>
+#include <xaml/ui/native_control.hpp>
+
+using namespace std;
+
+namespace xaml
+{
+    void progress::__draw(rectangle const& region)
+    {
+        if (!get_handle())
+        {
+            NSProgressIndicator* prog = [NSProgressIndicator new];
+            auto h = make_shared<native_control>();
+            h->handle = prog;
+            set_handle(h);
+            draw_progress();
+        }
+        __set_rect(region);
+    }
+
+    void progress::draw_progress()
+    {
+        NSProgressIndicator* prog = (NSProgressIndicator*)get_handle()->handle;
+        prog.minValue = (double)m_minimum;
+        prog.maxValue = (double)m_maximum;
+        prog.doubleValue = (double)m_value;
+    }
+}
