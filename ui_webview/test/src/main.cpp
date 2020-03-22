a2e482d3bee438e55043e66da665ef65983ab568
Berrysoft <Strawberry_Str@hotmail.com>
2020-03-22 20:59:28 +0800

    Use appmain for webview test.

--- ui_webview/test/src/main.cpp
+++ ui_webview/test/src/main.cpp
@@ -1,28 +1,14 @@
 #include <test_window.hpp>
-#include <xaml/ui/application.hpp>
-
-#if defined(WIN32) || defined(__MINGW32__)
-#include <Windows.h>
-#include <tchar.h>
-#endif // WIN32 || __MINGW32__
+#include <xaml/ui/appmain.hpp>
 
 using namespace std;
 using namespace xaml;
 using namespace xaml::test;
 
-#if defined(WIN32) || defined(__MINGW32__)
-INT APIENTRY _tWinMain(HINSTANCE, HINSTANCE, LPTSTR lpCmdLine, INT)
-#else
-int main(int argc, char const* const* argv)
-#endif // WIN32 || __MINGW32__
+XAML_APP_MAIN_START()
 {
-#if defined(WIN32) || defined(__MINGW32__)
-    auto app = application::init();
-#else
-    auto app = application::init(argc, argv);
-#endif // WIN32 || __MINGW32__
     auto wnd = make_shared<test_window>();
     wnd->init_components();
     wnd->show();
-    return app->run();
 }
+XAML_APP_MAIN_END()
