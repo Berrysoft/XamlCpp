e8fde6ff2dbba7b024b13b4f6c6192aab14cab85
Berrysoft <Strawberry_Str@hotmail.com>
2020-04-05 21:26:04 +0800

    Use imported libraries for find modules.

---
+++ cmake/Modules/FindWEBKIT4.cmake
@@ -0,0 +1,12 @@
+if(UNIX AND NOT APPLE)
+    find_package(PkgConfig REQUIRED)
+    pkg_check_modules(WEBKIT4 REQUIRED webkit2gtk-4.0)
+endif()
+
+if(${WEBKIT4_FOUND})
+    add_library(webkit4 INTERFACE IMPORTED)
+    target_link_directories(webkit4 INTERFACE ${WEBKIT4_LIBRARY_DIRS})
+    target_compile_definitions(webkit4 INTERFACE ${WEBKIT4_CFLAGS_OTHER})
+    target_include_directories(webkit4 INTERFACE ${WEBKIT4_INCLUDE_DIRS})
+    target_link_libraries(webkit4 INTERFACE ${WEBKIT4_LIBRARIES})
+endif()
--- cmake/Modules/FindWIL.cmake
+++ cmake/Modules/FindWIL.cmake
@@ -2,6 +2,8 @@ if(WIN32)
 	find_path(WIL_INCLUDE_DIRS wil)
 
 	message(STATUS "Found WIL")
-
 	set(WIL_FOUND TRUE)
+
+	add_library(wil INTERFACE IMPORTED)
+	target_include_directories(wil INTERFACE ${WIL_INCLUDE_DIRS})
 endif()
