5b714e536f9ef0adf0b843c2d11cef6db640e940
Berrysoft <Strawberry_Str@hotmail.com>
2020-04-17 21:13:29 +0800

    [WIP] type info.

---
+++ meta/include/xaml/meta/type_info.h
@@ -0,0 +1,16 @@
+#ifndef XAML_META_TYPE_INFO_H
+#define XAML_META_TYPE_INFO_H
+
+#include <xaml/meta/reflection_info.h>
+
+XAML_CLASS(xaml_type_info, { 0x3de3b2c1, 0x09d6, 0x433c, { 0xbf, 0x40, 0x40, 0x2d, 0xfe, 0x28, 0xda, 0x1d } })
+
+#ifdef __cplusplus
+struct xaml_type_info : xaml_reflection_info
+{
+};
+#else
+
+#endif // __cplusplus
+
+#endif // !XAML_META_TYPE_INFO_H
