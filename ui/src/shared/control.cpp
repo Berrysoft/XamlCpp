4b96b97fd81288fc67fca1f07d5031e9b3642a16
Berrysoft <Strawberry_Str@hotmail.com>
2020-04-21 16:44:34 +0800

    xaml_control_members

--- ui/src/shared/control.cpp
+++ ui/src/shared/control.cpp
@@ -3,14 +3,21 @@
 
 using namespace std;
 
-xaml_result xaml_control_register(xaml_meta_context* ctx) noexcept
+xaml_result xaml_control_members(xaml_type_info_registration* __info) noexcept
 {
-    XAML_TYPE_INFO_NEW(xaml_control, "xaml/ui/control.h");
+    using self_type = xaml_control;
     XAML_TYPE_INFO_ADD_PROP_EVENT(size);
     XAML_TYPE_INFO_ADD_PROP(width);
     XAML_TYPE_INFO_ADD_PROP(height);
     XAML_TYPE_INFO_ADD_PROP_EVENT(margin);
     XAML_TYPE_INFO_ADD_PROP_EVENT(halignment);
     XAML_TYPE_INFO_ADD_PROP_EVENT(valignment);
+    return XAML_S_OK;
+}
+
+xaml_result xaml_control_register(xaml_meta_context* ctx) noexcept
+{
+    XAML_TYPE_INFO_NEW(xaml_control, "xaml/ui/control.h");
+    XAML_RETURN_IF_FAILED(xaml_control_members(__info.get()));
     return ctx->add_type(__info.get());
 }
