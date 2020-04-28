91159e2f602be42c6eca27289fb242795456903b
Berrysoft <Strawberry_Str@hotmail.com>
2020-04-28 17:14:52 +0800

    xaml_password_entry.

--- ui_controls/include/xaml/ui/controls/password_entry.h
+++ ui_controls/include/xaml/ui/controls/password_entry.h
@@ -13,4 +13,8 @@ XAML_DECL_INTERFACE_(xaml_password_entry, xaml_entry)
     XAML_DECL_VTBL(xaml_password_entry, XAML_PASSWORD_ENTRY_VTBL);
 };
 
+EXTERN_C XAML_UI_CONTROLS_API xaml_result XAML_CALL xaml_password_entry_new(xaml_password_entry**) XAML_NOEXCEPT;
+EXTERN_C XAML_UI_CONTROLS_API xaml_result XAML_CALL xaml_password_entry_members(xaml_type_info_registration*) XAML_NOEXCEPT;
+EXTERN_C XAML_UI_CONTROLS_API xaml_result XAML_CALL xaml_password_entry_register(xaml_meta_context*) XAML_NOEXCEPT;
+
 #endif // !XAML_UI_CONTROLS_PASSWORD_ENTRY_H
--- ui_controls/src/shared/password_entry.cpp
+++ ui_controls/src/shared/password_entry.cpp
@@ -19,3 +19,24 @@ xaml_result xaml_password_entry_impl::init() noexcept
         },
         &token)));
 }
+
+xaml_result XAML_CALL xaml_password_entry_new(xaml_password_entry** ptr) noexcept
+{
+    return xaml_object_init<xaml_password_entry_impl>(ptr);
+}
+
+xaml_result XAML_CALL xaml_password_entry_members(xaml_type_info_registration* __info) noexcept
+{
+    using self_type = xaml_password_entry;
+    XAML_RETURN_IF_FAILED(xaml_entry_members(__info));
+    XAML_TYPE_INFO_ADD_CTOR(xaml_password_entry_new);
+    XAML_TYPE_INFO_ADD_PROP_EVENT(password_char);
+    return XAML_S_OK;
+}
+
+xaml_result XAML_CALL xaml_password_entry_register(xaml_meta_context* ctx) noexcept
+{
+    XAML_TYPE_INFO_NEW(xaml_password_entry, "xaml/ui/controls/password_entry.h");
+    XAML_RETURN_IF_FAILED(xaml_password_entry_members(__info.get()));
+    return ctx->add_type(__info.get());
+}
