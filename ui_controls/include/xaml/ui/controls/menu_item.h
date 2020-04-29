0f2f137246a3e994587b50ebcbc9ef7f74745834
Berrysoft <Strawberry_Str@hotmail.com>
2020-04-29 20:17:51 +0800

    [WIP] menu_item.

---
+++ ui_controls/include/xaml/ui/controls/menu_item.h
@@ -0,0 +1,63 @@
+#ifndef XAML_UI_CONTROLS_MENU_ITEM_H
+#define XAML_UI_CONTROLS_MENU_ITEM_H
+
+#include <xaml/ui/control.h>
+
+XAML_CLASS(xaml_menu_item, { 0xbaa16b2b, 0x1cca, 0x4a7c, { 0x8b, 0xbe, 0x96, 0x2b, 0x56, 0x95, 0x67, 0x7f } })
+
+#define XAML_MENU_ITEM_VTBL(type)               \
+    XAML_VTBL_INHERIT(XAML_CONTROL_VTBL(type)); \
+    XAML_PROP(text, type, xaml_string**, xaml_string*)
+
+XAML_DECL_INTERFACE_(xaml_menu_item, xaml_control)
+{
+    XAML_DECL_VTBL(xaml_menu_item, XAML_MENU_ITEM_VTBL);
+};
+
+XAML_CLASS(xaml_popup_menu_item, { 0xc1445529, 0x6932, 0x46a0, { 0xb9, 0x47, 0x2f, 0xe0, 0x04, 0x63, 0xa1, 0x27 } })
+
+#define XAML_POPUP_MENU_ITEM_VTBL(type)                 \
+    XAML_VTBL_INHERIT(XAML_MENU_ITEM_VTBL(type));       \
+    XAML_METHOD(get_submenu, type, xaml_vector_view**); \
+    XAML_CPROP(submenu, type, xaml_menu_item*, xaml_menu_item*)
+
+XAML_DECL_INTERFACE_(xaml_popup_menu_item, xaml_menu_item)
+{
+    XAML_DECL_VTBL(xaml_popup_menu_item, XAML_POPUP_MENU_ITEM_VTBL);
+};
+
+XAML_CLASS(xaml_check_menu_item, { 0xba31cca6, 0x5bb1, 0x4b90, { 0xab, 0x1a, 0x39, 0xcf, 0x13, 0x27, 0x6b, 0x2e } })
+
+#define XAML_CHECK_MENU_ITEM_VTBL(type)           \
+    XAML_VTBL_INHERIT(XAML_MENU_ITEM_VTBL(type)); \
+    XAML_PROP(is_checked, type, bool*, bool);     \
+    XAML_EVENT(is_checked_changed, type)
+
+XAML_DECL_INTERFACE_(xaml_check_menu_item, xaml_menu_item)
+{
+    XAML_DECL_VTBL(xaml_check_menu_item, XAML_CHECK_MENU_ITEM_VTBL);
+};
+
+XAML_CLASS(xaml_radio_menu_item, { 0x49494137, 0x05e4, 0x477f, { 0xa5, 0x2a, 0xef, 0x07, 0x4e, 0xca, 0x9c, 0x20 } })
+
+#define XAML_RADIO_MENU_ITEM_VTBL(type)           \
+    XAML_VTBL_INHERIT(XAML_MENU_ITEM_VTBL(type)); \
+    XAML_PROP(is_checked, type, bool*, bool);     \
+    XAML_EVENT(is_checked_changed, type);         \
+    XAML_PROP(group, type, xaml_string**, xaml_string*)
+
+XAML_DECL_INTERFACE_(xaml_radio_menu_item, xaml_menu_item)
+{
+    XAML_DECL_VTBL(xaml_radio_menu_item, XAML_RADIO_MENU_ITEM_VTBL);
+};
+
+XAML_CLASS(xaml_separator_menu_item, { 0xecc1f5ab, 0x5420, 0x45bb, { 0x8f, 0xb0, 0x2f, 0xa1, 0xea, 0xbb, 0xec, 0x24 } })
+
+#define XAML_SEPARATOR_MENU_ITEM_VTBL(type) XAML_VTBL_INHERIT(XAML_MENU_ITEM_VTBL(type))
+
+XAML_DECL_INTERFACE_(xaml_separator_menu_item, xaml_menu_item)
+{
+    XAML_DECL_VTBL(xaml_separator_menu_item, XAML_SEPARATOR_MENU_ITEM_VTBL);
+};
+
+#endif // !XAML_UI_CONTROLS_MENU_ITEM_H
--- ui_controls/src/shared/menu_item.cpp
+++ ui_controls/src/shared/menu_item.cpp
@@ -1,94 +1,94 @@
-#include <xaml/ui/controls/menu_item.hpp>
-#include <xaml/ui/menu_bar.hpp>
+#include <xaml/ui/controls/menu_item.h>
+#include <xaml/ui/menu_bar.h>
 
 using namespace std;
 
-namespace xaml
-{
-    menu_item::menu_item() : control() {}
-
-    menu_item::~menu_item() {}
-
-    popup_menu_item::popup_menu_item() : menu_item() {}
-
-    popup_menu_item::~popup_menu_item() {}
-
-    void popup_menu_item::add_submenu(shared_ptr<menu_item> const& child)
-    {
-        if (child)
-        {
-            auto it = find(m_submenu.begin(), m_submenu.end(), child);
-            if (it == m_submenu.end())
-            {
-                m_submenu.push_back(child);
-                child->set_parent(shared_from_this<control>());
-                __parent_redraw();
-            }
-        }
-    }
-
-    void popup_menu_item::remove_submenu(shared_ptr<menu_item> const& child)
-    {
-        auto it = find(m_submenu.begin(), m_submenu.end(), child);
-        if (it != m_submenu.end())
-        {
-            child->set_parent({});
-            m_submenu.erase(it);
-            __parent_redraw();
-        }
-    }
-
-    check_menu_item::check_menu_item() : menu_item()
-    {
-        add_is_checked_changed([this](shared_ptr<check_menu_item>, bool) { if(get_handle()) draw_checked(); });
-    }
-
-    check_menu_item::~check_menu_item() {}
-
-    radio_menu_item::radio_menu_item() : menu_item()
-    {
-        add_is_checked_changed([this](shared_ptr<radio_menu_item>, bool) {
-            if (get_handle())
-            {
-                draw_checked();
-                draw_group();
-            }
-        });
-    }
-
-    radio_menu_item::~radio_menu_item() {}
-
-#ifndef XAML_UI_GTK3
-    void radio_menu_item::draw_group()
-    {
-        if (auto sparent = get_parent().lock())
-        {
-            if (get_is_checked())
-            {
-                if (auto multic = sparent->query<menu_bar>())
-                {
-                    for (auto& c : multic->get_children())
-                    {
-                        if (c)
-                        {
-                            if (auto rc = c->query<radio_menu_item>())
-                            {
-                                if (c != shared_from_this<radio_menu_item>() && rc->get_group() == get_group())
-                                {
-                                    rc->set_is_checked(false);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-#endif // !XAML_UI_GTK3
-
-    separator_menu_item::separator_menu_item() : menu_item()
-    {
-    }
-
-    separator_menu_item::~separator_menu_item() {}
-} // namespace xaml
+//namespace xaml
+//{
+//    menu_item::menu_item() : control() {}
+//
+//    menu_item::~menu_item() {}
+//
+//    popup_menu_item::popup_menu_item() : menu_item() {}
+//
+//    popup_menu_item::~popup_menu_item() {}
+//
+//    void popup_menu_item::add_submenu(shared_ptr<menu_item> const& child)
+//    {
+//        if (child)
+//        {
+//            auto it = find(m_submenu.begin(), m_submenu.end(), child);
+//            if (it == m_submenu.end())
+//            {
+//                m_submenu.push_back(child);
+//                child->set_parent(shared_from_this<control>());
+//                __parent_redraw();
+//            }
+//        }
+//    }
+//
+//    void popup_menu_item::remove_submenu(shared_ptr<menu_item> const& child)
+//    {
+//        auto it = find(m_submenu.begin(), m_submenu.end(), child);
+//        if (it != m_submenu.end())
+//        {
+//            child->set_parent({});
+//            m_submenu.erase(it);
+//            __parent_redraw();
+//        }
+//    }
+//
+//    check_menu_item::check_menu_item() : menu_item()
+//    {
+//        add_is_checked_changed([this](shared_ptr<check_menu_item>, bool) { if(get_handle()) draw_checked(); });
+//    }
+//
+//    check_menu_item::~check_menu_item() {}
+//
+//    radio_menu_item::radio_menu_item() : menu_item()
+//    {
+//        add_is_checked_changed([this](shared_ptr<radio_menu_item>, bool) {
+//            if (get_handle())
+//            {
+//                draw_checked();
+//                draw_group();
+//            }
+//        });
+//    }
+//
+//    radio_menu_item::~radio_menu_item() {}
+//
+//#ifndef XAML_UI_GTK3
+//    void radio_menu_item::draw_group()
+//    {
+//        if (auto sparent = get_parent().lock())
+//        {
+//            if (get_is_checked())
+//            {
+//                if (auto multic = sparent->query<menu_bar>())
+//                {
+//                    for (auto& c : multic->get_children())
+//                    {
+//                        if (c)
+//                        {
+//                            if (auto rc = c->query<radio_menu_item>())
+//                            {
+//                                if (c != shared_from_this<radio_menu_item>() && rc->get_group() == get_group())
+//                                {
+//                                    rc->set_is_checked(false);
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+//#endif // !XAML_UI_GTK3
+//
+//    separator_menu_item::separator_menu_item() : menu_item()
+//    {
+//    }
+//
+//    separator_menu_item::~separator_menu_item() {}
+//} // namespace xaml
