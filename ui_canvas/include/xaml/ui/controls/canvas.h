db7bd988c39da94f96a44a5159d3792ddb89c882
Berrysoft <Strawberry_Str@hotmail.com>
2020-04-30 23:05:42 +0800

    [WIP] xaml_canvas.

---
+++ ui_canvas/include/xaml/ui/controls/canvas.h
@@ -0,0 +1,12 @@
+#ifndef XAML_UI_CANVAS_CANVAS_H
+#define XAML_UI_CANVAS_CANVAS_H
+
+#include <xaml/ui/control.h>
+#include <xaml/ui/drawing.h>
+
+typedef struct xaml_drawing_brush
+{
+    xaml_color fill;
+} xaml_drawing_brush;
+
+#endif // !XAML_UI_CANVAS_CANVAS_H
--- ui_canvas/src/shared/canvas.cpp
+++ ui_canvas/src/shared/canvas.cpp
@@ -1,12 +1,12 @@
-#include <xaml/ui/controls/canvas.hpp>
+#include <xaml/ui/controls/canvas.h>
 
 using namespace std;
 
-namespace xaml
-{
-    canvas::canvas() : control() {}
-
-    canvas::~canvas() {}
-
-    drawing_context::drawing_context(native_handle_type handle) : m_handle(handle) {}
-} // namespace xaml
+//namespace xaml
+//{
+//    canvas::canvas() : control() {}
+//
+//    canvas::~canvas() {}
+//
+//    drawing_context::drawing_context(native_handle_type handle) : m_handle(handle) {}
+//} // namespace xaml
