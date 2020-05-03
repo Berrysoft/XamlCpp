0b5b4caea1f768c58f2c8ed843eb54c1bf6cc70d
Berrysoft <Strawberry_Str@hotmail.com>
2020-05-03 22:33:46 +0800

    xaml_application

--- ui/src/gtk3/application.cpp
+++ ui/src/gtk3/application.cpp
@@ -1,7 +1,7 @@
 #include <cstdlib>
 #include <gtk/gtk.h>
-#include <gtk3/resources.hpp>
-#include <xaml/ui/application.hpp>
+#include <shared/application.hpp>
+#include <xaml/ui/application.h>
 
 #ifdef XAML_WIN32
 #include <Windows.h>
@@ -9,37 +9,42 @@
 
 using namespace std;
 
-namespace xaml
+xaml_result xaml_application_impl::init(int argc, xaml_char_t** argv) noexcept
 {
-    application::application(int argc, char_t const* const* argv)
+    gtk_init(&argc, &argv);
+    XAML_RETURN_IF_FAILED(xaml_vector_new(&m_cmd_lines));
+    for (int i = 0; i < argc; i++)
     {
-        gtk_init(&argc, const_cast<char***>(&argv));
-        m_cmd_lines.assign(argv, argv + argc);
+        XAML_RETURN_IF_FAILED(m_cmd_lines->append(to_xaml_string(argv[i]).get()));
     }
+    return XAML_S_OK;
+}
 
-    int application::run()
-    {
-        gtk_main();
-        return m_quit_value;
-    }
+xaml_result xaml_application_impl::run(int* pres) noexcept
+{
+    gtk_main();
+    *pres = m_quit_value;
+    return XAML_S_OK;
+}
 
-    void application::quit(int value)
-    {
-        m_quit_value = value;
-        gtk_main_quit();
-    }
+xaml_result xaml_application_impl::quit(int value) noexcept
+{
+    m_quit_value = value;
+    gtk_main_quit();
+    return XAML_S_OK;
+}
 
-    application_theme application::get_theme() const
-    {
+xaml_result xaml_application_impl::get_theme(xaml_application_theme* ptheme) noexcept
+{
 #if defined(WIN32) || defined(__MINGW32__)
-        string theme(32767, L'\0');
-        DWORD count = GetEnvironmentVariable("GTK_THEME", theme.data(), (DWORD)theme.length());
-        theme.resize(count);
+    string theme(32767, L'\0');
+    DWORD count = GetEnvironmentVariable("GTK_THEME", theme.data(), (DWORD)theme.length());
+    theme.resize(count);
 #else
-        char* ptheme = getenv("GTK_THEME");
-        string_view theme = ptheme ? ptheme : string_view{};
+    char* ptheme = getenv("GTK_THEME");
+    string_view theme = ptheme ? ptheme : string_view{};
 #endif // WIN32 || __MINGW32__
 
-        return theme.ends_with(":dark") ? application_theme::dark : application_theme::light;
-    }
-} // namespace xaml
+    *ptheme = theme.ends_with(":dark") ? xaml_application_theme_dark : xaml_application_theme_light;
+    return XAML_S_OK;
+}
