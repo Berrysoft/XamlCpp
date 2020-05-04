7096f9b97478971ab08e71421ec71c6c3978fc33
Berrysoft <Strawberry_Str@hotmail.com>
2020-05-04 23:17:13 +0800

    xaml_check_box

--- ui_controls/src/gtk3/check_box.cpp
+++ ui_controls/src/gtk3/check_box.cpp
@@ -1,33 +1,28 @@
-#include <xaml/ui/controls/check_box.hpp>
-#include <xaml/ui/native_control.hpp>
+#include <shared/check_box.hpp>
+#include <xaml/ui/controls/check_box.h>
 
 using namespace std;
 
-namespace xaml
+xaml_result xaml_check_box_internal::draw(xaml_rectangle const& region) noexcept
 {
-    void check_box::__draw(rectangle const& region)
+    if (!m_handle)
     {
-        if (!get_handle())
-        {
-            auto h = make_shared<native_control>();
-            h->handle = gtk_check_button_new();
-            set_handle(h);
-            g_signal_connect(G_OBJECT(get_handle()->handle), "clicked", G_CALLBACK(button::on_clicked), this);
-            g_signal_connect(G_OBJECT(get_handle()->handle), "toggled", G_CALLBACK(check_box::on_toggled), this);
-            draw_text();
-            draw_checked();
-        }
-        button::__draw(region);
+        m_handle = gtk_check_button_new();
+        g_signal_connect(G_OBJECT(m_handle), "clicked", G_CALLBACK(xaml_button_internal::on_clicked), this);
+        g_signal_connect(G_OBJECT(m_handle), "toggled", G_CALLBACK(xaml_check_box_internal::on_toggled), this);
+        XAML_RETURN_IF_FAILED(draw_text());
+        XAML_RETURN_IF_FAILED(draw_checked());
     }
+    return set_rect(region);
+}
 
-    void check_box::draw_checked()
-    {
-        gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(get_handle()->handle), m_is_checked ? TRUE : FALSE);
-    }
+xaml_result xaml_check_box_internal::draw_checked() noexcept
+{
+    gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(m_handle), m_is_checked);
+    return XAML_S_OK;
+}
 
-    void check_box::on_toggled(void*, void* data)
-    {
-        check_box* s = (check_box*)data;
-        s->set_is_checked(gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(s->get_handle()->handle)));
-    }
-} // namespace xaml
+void xaml_check_box_internal::on_toggled(GtkWidget*, xaml_check_box_internal* self) noexcept
+{
+    XAML_ASSERT_SUCCESS(self->set_is_checked(gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(self->m_handle))));
+}
