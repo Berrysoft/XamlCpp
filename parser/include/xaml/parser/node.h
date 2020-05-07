f18e3eab89b6e27bfdc5d0eb61ae85f1ef947ec5
Berrysoft <Strawberry_Str@hotmail.com>
2020-05-07 13:57:04 +0800

    [WIP] xaml_node

--- parser/include/xaml/parser/node.h
+++ parser/include/xaml/parser/node.h
@@ -29,6 +29,8 @@ XAML_DECL_INTERFACE_(xaml_string_node, xaml_node_base)
     XAML_DECL_VTBL(xaml_string_node, XAML_STRING_NODE_VTBL);
 };
 
+EXTERN_C XAML_PARSER_API xaml_result XAML_CALL xaml_string_node_new(xaml_type_info*, xaml_string*, xaml_string_node**) XAML_NOEXCEPT;
+
 XAML_CLASS(xaml_markup_node, { 0xa45401da, 0xa28d, 0x48bd, { 0x90, 0x4f, 0x7c, 0x53, 0x68, 0x04, 0x0f, 0xec } })
 
 #define XAML_MARKUP_NODE_VTBL(type)               \
@@ -39,6 +41,8 @@ XAML_DECL_INTERFACE_(xaml_markup_node, xaml_node_base)
 {
     XAML_DECL_VTBL(xaml_markup_node, XAML_MARKUP_NODE_VTBL);
 };
+
+EXTERN_C XAML_PARSER_API xaml_result XAML_CALL xaml_markup_node_new(xaml_type_info*, xaml_string*, xaml_markup_node**) XAML_NOEXCEPT;
 
 XAML_CLASS(xaml_node, { 0x85ad34c8, 0x3cba, 0x44eb, { 0x98, 0x1c, 0x67, 0xf7, 0x13, 0xd3, 0xe5, 0xeb } })
 
@@ -54,6 +58,8 @@ XAML_DECL_INTERFACE_(xaml_node, xaml_node_base)
     XAML_DECL_VTBL(xaml_node, XAML_NODE_VTBL);
 };
 
+EXTERN_C XAML_PARSER_API xaml_result XAML_CALL xaml_node_new(xaml_type_info*, xaml_string*, xaml_node**) XAML_NOEXCEPT;
+
 XAML_CLASS(xaml_attribute_event, { 0x79d5dc01, 0xf5e7, 0x40b5, { 0x9b, 0xd6, 0x1b, 0x76, 0x60, 0x68, 0x77, 0x96 } })
 
 #define XAML_ATTRIBUTE_EVENT_VTBL(type)             \
@@ -65,6 +71,8 @@ XAML_DECL_INTERFACE_(xaml_attribute_event, xaml_object)
 {
     XAML_DECL_VTBL(xaml_attribute_event, XAML_ATTRIBUTE_EVENT_VTBL);
 };
+
+EXTERN_C XAML_PARSER_API xaml_result XAML_CALL xaml_attribute_event_new(xaml_event_info*, xaml_string*, xaml_attribute_event**) XAML_NOEXCEPT;
 
 XAML_CLASS(xaml_attribute_property, { 0x82c20c0f, 0xc1cd, 0x418a, { 0x84, 0x76, 0x73, 0xd2, 0x6e, 0xf3, 0x15, 0x0a } })
 
@@ -79,6 +87,8 @@ XAML_DECL_INTERFACE_(xaml_attribute_property, xaml_object)
     XAML_DECL_VTBL(xaml_attribute_property, XAML_ATTRIBUTE_PROPERTY_VTBL);
 };
 
+EXTERN_C XAML_PARSER_API xaml_result XAML_CALL xaml_attribute_property_new(xaml_type_info*, xaml_property_info*, xaml_node_base*, xaml_attribute_property**) XAML_NOEXCEPT;
+
 XAML_CLASS(xaml_attribute_collection_property, { 0x9e34786c, 0x06f8, 0x493c, { 0xa5, 0xb4, 0x39, 0x8f, 0x28, 0x70, 0x04, 0x79 } })
 
 #define XAML_ATTRIBUTE_COLLECTION_PROPERTY_VTBL(type)  \
@@ -90,5 +100,7 @@ XAML_DECL_INTERFACE_(xaml_attribute_collection_property, xaml_object)
 {
     XAML_DECL_VTBL(xaml_attribute_collection_property, XAML_ATTRIBUTE_COLLECTION_PROPERTY_VTBL);
 };
+
+EXTERN_C XAML_PARSER_API xaml_result XAML_CALL xaml_attribute_collection_property_new(xaml_type_info*, xaml_property_info*, xaml_vector_view*, xaml_attribute_collection_property**) XAML_NOEXCEPT;
 
 #endif // !XAML_PARSER_NODE_H
