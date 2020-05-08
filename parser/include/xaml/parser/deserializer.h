8b5099d8f91137aae9e26a912ef350cdcd8f39f4
Berrysoft <Strawberry_Str@hotmail.com>
2020-05-08 16:41:41 +0800

    [WIP] deserializer

--- parser/include/xaml/parser/deserializer.h
+++ parser/include/xaml/parser/deserializer.h
@@ -1,10 +1,11 @@
 #ifndef XAML_PARSER_DESERIALIZER_H
 #define XAML_PARSER_DESERIALIZER_H
 
+#include <xaml/meta/meta_context.h>
 #include <xaml/parser/node.h>
 
-EXTERN_C XAML_PARSER_API xaml_result XAML_CALL xaml_parser_deserialize(xaml_node*, xaml_object**) XAML_NOEXCEPT;
-EXTERN_C XAML_PARSER_API xaml_result XAML_CALL xaml_parser_deserialize_inplace(xaml_node*, xaml_object*) XAML_NOEXCEPT;
-EXTERN_C XAML_PARSER_API xaml_result XAML_CALL xaml_parser_deserialize_markup(xaml_markup_node*, xaml_object**) XAML_NOEXCEPT;
+EXTERN_C XAML_PARSER_API xaml_result XAML_CALL xaml_parser_deserialize(xaml_meta_context*, xaml_node*, xaml_object**) XAML_NOEXCEPT;
+EXTERN_C XAML_PARSER_API xaml_result XAML_CALL xaml_parser_deserialize_inplace(xaml_meta_context*, xaml_node*, xaml_object*) XAML_NOEXCEPT;
+EXTERN_C XAML_PARSER_API xaml_result XAML_CALL xaml_parser_deserialize_markup(xaml_meta_context*, xaml_markup_node*, xaml_object**) XAML_NOEXCEPT;
 
 #endif // !XAML_PARSER_DESERIALIZER_H
