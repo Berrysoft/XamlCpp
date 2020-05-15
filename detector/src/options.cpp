ffa49f93cdf173b3c37eeaa61d987ae8c11c457c
Berrysoft <Strawberry_Str@hotmail.com>
2020-05-15 15:18:23 +0800

    Fix depends.

--- detector/src/options.cpp
+++ detector/src/options.cpp
@@ -24,7 +24,9 @@ xaml_result XAML_CALL xaml_detector_options_register(xaml_meta_context* ctx) noe
     xaml_ptr<xaml_cmdline_option> opt;
     XAML_RETURN_IF_FAILED(xaml_cmdline_option_new(&opt));
     XAML_RETURN_IF_FAILED(opt->add_arg(U('h'), U("help"), U("help"), U("Print help message")));
-    XAML_RETURN_IF_FAILED(opt->add_arg(U('v'), U("verbose"), U("verbose"), U("Show detailed output")));
+#ifndef NDEBUG
+    XAML_RETURN_IF_FAILED(opt->add_arg(U('d'), U("debug"), U("debug"), U("Show debug output")));
+#endif // !NDEBUG
     XAML_RETURN_IF_FAILED(opt->add_arg(U('r'), U("recursive"), U("recursive"), U("Load modules recursively")));
     XAML_RETURN_IF_FAILED(opt->add_arg(0, U("no-logo"), U("no_logo"), U("Cancellation to show copyright infomation")));
     XAML_RETURN_IF_FAILED(opt->add_arg(0, {}, U("path"), U("Library path")));
--- resource_compiler/src/options.cpp
+++ resource_compiler/src/options.cpp
@@ -46,7 +46,9 @@ xaml_result XAML_CALL xaml_rc_options_register(xaml_meta_context* ctx) noexcept
     xaml_ptr<xaml_cmdline_option> opt;
     XAML_RETURN_IF_FAILED(xaml_cmdline_option_new(&opt));
     XAML_RETURN_IF_FAILED(opt->add_arg(U('h'), U("help"), U("help"), U("Print help message")));
-    XAML_RETURN_IF_FAILED(opt->add_arg(U('v'), U("verbose"), U("verbose"), U("Show detailed output")));
+#ifndef NDEBUG
+    XAML_RETURN_IF_FAILED(opt->add_arg(U('d'), U("debug"), U("debug"), U("Show debug output")));
+#endif // !NDEBUG
     XAML_RETURN_IF_FAILED(opt->add_arg(0, U("no-logo"), U("no_logo"), U("Cancellation to show copyright infomation")));
     XAML_RETURN_IF_FAILED(opt->add_arg(U('o'), U("output"), U("output"), U("Output file")));
     XAML_RETURN_IF_FAILED(opt->add_arg(0, {}, U("input"), U("Input files")));
