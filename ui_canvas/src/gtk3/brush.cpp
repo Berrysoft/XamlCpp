b5ba0b085e9dd6ba5d9ff8dc8da0cfe4ed648727
Berrysoft <Strawberry_Str@hotmail.com>
2020-05-29 23:40:44 +0800

    Add radial gradient for gtk3.

--- ui_canvas/src/gtk3/brush.cpp
+++ ui_canvas/src/gtk3/brush.cpp
@@ -11,6 +11,18 @@ xaml_result xaml_solid_brush_impl::set(cairo_t* handle, xaml_rectangle const&) n
     return XAML_S_OK;
 }
 
+static xaml_result add_stops(cairo_pattern_t* pattern, xaml_ptr<xaml_vector> const& stops) noexcept
+{
+    XAML_FOREACH_START(item, stops);
+    {
+        xaml_gradient_stop const* pitem;
+        XAML_RETURN_IF_FAILED(item.query<xaml_box>()->get_value_ptr(&pitem));
+        cairo_pattern_add_color_stop_rgba(pattern, pitem->position, pitem->color.r / 255.0, pitem->color.g / 255.0, pitem->color.b / 255.0, pitem->color.a / 255.0);
+    }
+    XAML_FOREACH_END();
+    return XAML_S_OK;
+}
+
 xaml_result xaml_linear_gradient_brush_impl::set(cairo_t* handle, xaml_rectangle const& region) noexcept
 {
     unique_ptr<cairo_pattern_t, g_free_deleter<cairo_pattern_t, cairo_pattern_destroy>> pattern{
@@ -20,13 +32,26 @@ xaml_result xaml_linear_gradient_brush_impl::set(cairo_t* handle, xaml_rectangle
             lerp(region.x, region.x + region.width, m_end_point.x),
             lerp(region.y, region.y + region.height, m_end_point.y))
     };
-    XAML_FOREACH_START(item, m_gradient_stops);
-    {
-        xaml_gradient_stop const* pitem;
-        XAML_RETURN_IF_FAILED(item.query<xaml_box>()->get_value_ptr(&pitem));
-        cairo_pattern_add_color_stop_rgba(pattern.get(), pitem->position, pitem->color.r / 255.0, pitem->color.g / 255.0, pitem->color.b / 255.0, pitem->color.a / 255.0);
-    }
-    XAML_FOREACH_END();
+    add_stops(pattern.get(), m_gradient_stops);
+    cairo_set_source(handle, pattern.get());
+    return XAML_S_OK;
+}
+
+xaml_result xaml_radial_gradient_brush_impl::set(cairo_t* handle, xaml_rectangle const& region) noexcept
+{
+    unique_ptr<cairo_pattern_t, g_free_deleter<cairo_pattern_t, cairo_pattern_destroy>> pattern{
+        cairo_pattern_create_radial(
+            lerp(region.x, region.x + region.width, m_origin.x),
+            lerp(region.y, region.y + region.height, m_origin.y),
+            0,
+            lerp(region.x, region.x + region.width, m_center.x),
+            lerp(region.y, region.y + region.height, m_center.y),
+            m_radius.width * region.width)
+    };
+    cairo_matrix_t matrix;
+    cairo_matrix_init_scale(&matrix, 1, m_radius.height / m_radius.width);
+    cairo_pattern_set_matrix(pattern.get(), &matrix);
+    add_stops(pattern.get(), m_gradient_stops);
     cairo_set_source(handle, pattern.get());
     return XAML_S_OK;
 }
