dbc83437f49a64f1f4267a222694ba62b6b21c41
Berrysoft <Strawberry_Str@hotmail.com>
2020-07-12 22:44:45 +0800

    Add error for other standard libraries.

--- helpers/include/xaml/internal/stream.hpp
+++ helpers/include/xaml/internal/stream.hpp
@@ -11,8 +11,10 @@
     #include <fstream>
 #elif defined(__GLIBCXX__)
     #include <ext/stdio_filebuf.h>
-#else
+#elif defined(_LIBCPP_VERSION)
     #include <__std_stream>
+#else
+    #error Cannot determine standard library
 #endif
 
 template <typename Char, typename F, typename... Args>
@@ -20,12 +22,15 @@ decltype(auto) cfile_ostream_invoke(F&& f, std::FILE* file, Args&&... args) noex
     std::forward<F>(f)(std::declval<std::basic_ostream<Char>&>(), std::forward<Args>(args)...)))
 {
 #ifdef XAML_WIN32
-    switch (_fileno(file))
+    if constexpr (std::is_same_v<Char, char>)
     {
-    case 1:
-        return std::forward<F>(f)(nowide::cout, std::forward<Args>(args)...);
-    case 2:
-        return std::forward<F>(f)(nowide::cerr, std::forward<Args>(args)...);
+        switch (_fileno(file))
+        {
+        case 1:
+            return std::forward<F>(f)(nowide::cout, std::forward<Args>(args)...);
+        case 2:
+            return std::forward<F>(f)(nowide::cerr, std::forward<Args>(args)...);
+        }
     }
 #endif // XAML_WIN32
 
@@ -33,7 +38,7 @@ decltype(auto) cfile_ostream_invoke(F&& f, std::FILE* file, Args&&... args) noex
     std::basic_filebuf<Char> buf{ file };
 #elif defined(__GLIBCXX__)
     __gnu_cxx::stdio_filebuf<Char> buf{ file, std::ios_base::out };
-#else
+#elif defined(_LIBCPP_VERSION)
     std::mbstate_t state;
     std::__stdoutbuf<Char> buf{ file, &state };
 #endif
@@ -47,10 +52,13 @@ decltype(auto) cfile_istream_invoke(F&& f, std::FILE* file, Args&&... args) noex
     std::forward<F>(f)(std::declval<std::basic_istream<Char>&>(), std::forward<Args>(args)...)))
 {
 #ifdef XAML_WIN32
-    switch (_fileno(file))
+    if constexpr (std::is_same_v<Char, char>)
     {
-    case 0:
-        return std::forward<F>(f)(nowide::cin, std::forward<Args>(args)...);
+        switch (_fileno(file))
+        {
+        case 0:
+            return std::forward<F>(f)(nowide::cin, std::forward<Args>(args)...);
+        }
     }
 #endif // XAML_WIN32
 
@@ -58,7 +66,7 @@ decltype(auto) cfile_istream_invoke(F&& f, std::FILE* file, Args&&... args) noex
     std::basic_filebuf<Char> buf{ file };
 #elif defined(__GLIBCXX__)
     __gnu_cxx::stdio_filebuf<Char> buf{ file, std::ios_base::in };
-#else
+#elif defined(_LIBCPP_VERSION)
     std::mbstate_t state;
     std::__stdinbuf<Char> buf{ file, &state };
 #endif
