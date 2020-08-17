b9481d96f3f2b638578ecd863a2674a6dad70b13
Berrysoft <Strawberry_Str@hotmail.com>
2020-08-17 10:56:51 +0800

    Add coinitialize for appmain.

--- ui/appmain/src/appmain.cpp
+++ ui/appmain/src/appmain.cpp
@@ -3,6 +3,10 @@
 #include <sf/format.hpp>
 #include <xaml/ui/application.h>
 
+#ifdef XAML_UI_WINDOWS
+    #include <combaseapi.h>
+#endif // XAML_UI_WINDOWS
+
 using namespace std;
 
 static int xaml_main_end(xaml_result hr, int* pcode) noexcept
@@ -22,7 +26,20 @@ static xaml_result xaml_main_begin(int argc, char** argv, int* pcode) noexcept
 {
     xaml_ptr<xaml_application> app;
     XAML_RETURN_IF_FAILED(xaml_application_init_with_args(argc, argv, &app));
-    return xaml_main(app, pcode);
+#ifdef XAML_UI_WINDOWS
+    {
+        HRESULT hr = CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+        if (FAILED(hr))
+        {
+            xaml_result_raise_message(hr, xaml_result_raise_warning, "Cannot initialize COM.");
+        }
+    }
+#endif // XAML_UI_WINDOWS
+    xaml_result hr = xaml_main(app, pcode);
+#ifdef XAML_UI_WINDOWS
+    CoUninitialize();
+#endif // XAML_UI_WINDOWS
+    return hr;
 }
 
 int main(int argc, char** argv)
