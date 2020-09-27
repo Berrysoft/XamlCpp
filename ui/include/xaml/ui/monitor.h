29cb47f2627f62df4d82513e5e88f8b3c384b3cc
Berrysoft <Strawberry_Str@hotmail.com>
2020-09-27 18:29:31 +0800

    Fix monitor.

--- ui/include/xaml/ui/monitor.h
+++ ui/include/xaml/ui/monitor.h
@@ -16,6 +16,16 @@ struct xaml_monitor
 
 XAML_TYPE(xaml_monitor, { 0xc81625a5, 0xb58f, 0x403e, { 0x92, 0xbb, 0x1a, 0xe2, 0xdf, 0x12, 0x4d, 0xca } })
 
-EXTERN_C XAML_UI_API xaml_result XAML_CALL xaml_monitor_get_all(xaml_vector_view**) XAML_NOEXCEPT;
+#ifndef xaml_enumerator__xaml_monitor__defined
+    #define xaml_enumerator__xaml_monitor__defined
+XAML_ENUMERATOR_T_TYPE(XAML_T_V(xaml_monitor))
+#endif // !xaml_enumerator__xaml_monitor__defined
+
+#ifndef xaml_vector_view__xaml_monitor__defined
+    #define xaml_vector_view__xaml_monitor__defined
+XAML_VECTOR_VIEW_T_TYPE(XAML_T_V(xaml_monitor))
+#endif // !xaml_vector_view__xaml_monitor__defined
+
+EXTERN_C XAML_UI_API xaml_result XAML_CALL xaml_monitor_get_all(XAML_VECTOR_VIEW_T_NAME(xaml_monitor) **) XAML_NOEXCEPT;
 
 #endif // !XAML_UI_SCREEN_H
